35640207d169df1fcb1b5bb45045e50eb7b0b274
fahriTrh <fakhrialauddin44@gmail.com>
2025-03-03 10:42:36 +0700

    fixed error

--- src/components/ui/question-container.tsx
+++ src/components/ui/question-container.tsx
@@ -4,10 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
-    CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
 
 import { RefreshCcw } from 'lucide-react';
