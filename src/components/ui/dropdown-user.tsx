3b0d68dcf1662b14b4da895aca41a976eb2207c5
fahriTrh <fakhrialauddin44@gmail.com>
2025-04-11 22:04:26 +0700

    add authentication

--- src/components/ui/dropdown-user.tsx
+++ src/components/ui/dropdown-user.tsx
@@ -1,14 +1,22 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { User } from 'lucide-react';
-import { LogOut } from 'lucide-react';
+import { LogOut, LogIn } from 'lucide-react';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu" 
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 
 export default function DropdownUser() {
+
+    const { data: session } = useSession()
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -19,14 +27,24 @@ export default function DropdownUser() {
                     <User />
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem>
-                    user@gmail.com
-                </DropdownMenuItem>
-                <DropdownMenuItem className="hover:cursor-pointer">
-                    Logout <LogOut />
-                </DropdownMenuItem>
-            </DropdownMenuContent>
+            {
+                session ? (
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>
+                            {session.user?.email}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => signOut()} className="hover:cursor-pointer">
+                            Logout <LogOut />
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                ) : (
+                    <DropdownMenuContent>
+                        <DropdownMenuItem onClick={() => signIn()} className="hover:cursor-pointer">
+                            Login <LogIn />
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                )
+            }
         </DropdownMenu>
     )
 }
